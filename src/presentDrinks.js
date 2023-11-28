f6991949108427db085ac77f35f3d68bf41f913f
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-28 11:34:46 +0700

    Display drinks function finished

--- src/presentDrinks.js
+++ src/presentDrinks.js
@@ -1,10 +1,11 @@
+import displayDrinks from "./displayDrinks.js";
 import fetchDrinks from "./fetchDrinks.js";
 
 const showDrinks = async (url) => {
   // fetch drinks
   const drinks = await fetchDrinks(url);
-  console.log(drinks);
   // display drinks
+  const section = displayDrinks(drinks);
 };
 
 export default showDrinks;
