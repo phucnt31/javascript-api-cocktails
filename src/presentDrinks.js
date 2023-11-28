ff55b716bbd0f452e55b3701a6261b630ba2dc27
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-28 12:07:44 +0700

    Setup getting single drink

--- src/presentDrinks.js
+++ src/presentDrinks.js
@@ -1,11 +1,15 @@
 import displayDrinks from "./displayDrinks.js";
 import fetchDrinks from "./fetchDrinks.js";
+import setDrink from "./setDrink.js";
 
 const showDrinks = async (url) => {
   // fetch drinks
   const drinks = await fetchDrinks(url);
   // display drinks
-  const section = displayDrinks(drinks);
+  const section = await displayDrinks(drinks);
+  if (section) {
+    setDrink(section);
+  }
 };
 
 export default showDrinks;
