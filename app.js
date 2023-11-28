e972c029404f7b7c68ea58c1fec0c99c0a751e31
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-28 11:13:21 +0700

    Setup present drinks

---
+++ app.js
@@ -0,0 +1,5 @@
+import showDrinks from "./src/presentDrinks.js";
+
+const URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a";
+
+window.addEventListener("DOMContentLoaded", () => showDrinks(URL));
